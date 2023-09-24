9cd31b20fb6c304e10b809646f5c6e76f0aaeeab
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-09-23 22:07:32 -0500

    hw_2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 'use strict'
+// let number = 12345;
+// let digits = number.toString().split('').join(' ')
+// console.log(digits)
+// function add (a, b) {
+//     return a + b ;
+// }
+// function subtract (a, b) {
+//     return a - b ;
+// }
+// function multiply (a, b) {
+//     return a * b ;
+// }
+// function divide (a, b) {
+//     if (b === 0) {
+//         return 'cannot be devided by zero'
+//     }
+//     return a / b ;
+// }
+// let num1 = 5;
+// let num2 = 10;
+//
+//
+// console.log(`Додавання: ${num1} + ${num2} = ${add(num1, num2)}`);
+// console.log(`Віднімання: ${num1} - ${num2} = ${subtract(num1, num2)}`);
+// console.log(`Множення: ${num1} * ${num2} = ${multiply(num1, num2)}`);
+// console.log(`Ділення: ${num1} / ${num2} = ${divide(num1, num2)}`);
+// function yourName (){
+//     let askName = prompt('your name')
+//     if (askName){
+//         let chanegedFirstLetter = askName.charAt(0).toUpperCase() + askName.slice(1)
+//         alert ( `yor name is ${chanegedFirstLetter}`)
+//     }else {
+//         alert('no');
+//     }
+// }
+// yourName ();
