ddc940104feddf82c3183bf2cf6fef7a97ab73d2
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-09-24 22:09:06 -0500

    hw_1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 'use strict'
-// console.log(typeof 1, typeof "string", typeof true, typeof undefined, typeof null, typeof Symbol(), typeof {}, typeof [], typeof function () {});
+const bigIntNumber = 1234567890123456789012345678901234567890n;
+console.log(typeof 1, typeof "string", typeof true, typeof undefined, typeof null, typeof Symbol(), typeof {}, typeof [], typeof function () {}, typeof bigIntNumber);
