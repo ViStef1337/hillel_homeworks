60e3c71c8079ee924eed5365031d97a22d5db707
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-09-24 22:02:36 -0500

    hw_3

--- js/script.js
+++ js/script.js
@@ -1,31 +1,96 @@
 'use strict'
+// 1
 //     alert("Tell me your name");
 //     let name = prompt("Your name");
 //     alert(`Hi ${name}`);
-function userInfo (){
-    let birthYear = Number(prompt('year of birth'))
-    let city = prompt('where do you live? ')
-    let sport = prompt('what is your favorite sport? ')
-    birthYear = 2023 - birthYear;
-    if (!birthYear) {
-        alert("it is sad you didnt want to note your info about your birth date.");
-        return;
-    } else if (!city) {
-        alert("it is sad you didnt want to note your info about your city.");
-        return;
-    } else if (!sport) {
-        alert("it is sad you didnt want to note your info about your sport.");
-        return;
-    }
-    if  (city == "Kiev") {
-        alert('you live in the capital of Ukraine')
-    }else if (city == "Washington") {
-        alert('you live in the capital of US')
-    }else if (city == "London") {
-        alert('you live in the capital of UK')
-    }else {
-        alert(`you live in ${city}`)
-    }
-    alert(`${birthYear},${city},${sport}`);
-}
-userInfo();
+
+
+
+// 2
+
+
+
+// let number = prompt("Give me your number")
+// let a = parseInt(number[0])
+// let b = parseInt(number[1])
+// let c = parseInt(number[2])
+// if (a === b && b === c) {
+//     alert("all digits are the same")
+// }else if (a === b || a === c || b === c) {
+//     alert('there are same digits')
+// }
+
+
+
+// 3
+
+
+
+// function userInfo() {
+//     let birthYear = Number(prompt('year of birth'))
+//     let city = prompt('where do you live? ')
+//     let sport = prompt('what is your favorite sport?')
+//
+//     if (birthYear) {
+//         calcYear = 2023 - birthYear;
+//     }
+//
+//     if (!birthYear && !city && !sport) {
+//         alert("No info");
+//         return;
+//     }
+//
+//     if (birthYear && city && !sport) {
+//         alert("You have not given sport")
+//         alert(`Years: ${calcYear}\nCity: ${city}`)
+//     }
+//
+//     if (birthYear && !city && sport) {
+//         alert("You have not given city")
+//         alert(`Years: ${calcYear}\nSport: ${sport}`)
+//     }
+//
+//     if (birthYear && !city && !sport) {
+//         alert("You have not given city and sport")
+//         alert(`Years: ${calcYear}`)
+//     }
+//
+//     if (!birthYear && city && sport) {
+//         alert("You have not given birthYear")
+//         alert(`City: ${city}\nSport: ${sport}`)
+//     }
+//
+//     if (!birthYear && city && !sport) {
+//         alert("You have not given birthYear and sport")
+//         alert(`City: ${city}`)
+//     }
+//
+//     if (!birthYear && !city && sport) {
+//         alert("You have not given birthYear and city")
+//         alert(`${sport}`)
+//     }
+//
+//
+//     if (birthYear && city && sport) {
+//         alert(`Years: ${calcYear}\nCity: ${city}\nSport: ${sport}`)
+//     }
+//
+//
+//
+//
+//     if  (city == "Kiev") {
+//         alert('you live in the capital of Ukraine')
+//     }   else if (city == "Washington") {
+//         alert('you live in the capital of US')
+//     }   else if (city == "London") {
+//         alert('you live in the capital of UK')
+//     } else if (city == null) {
+//
+//     } else if (city) {
+//         alert(`your city is ${city}`)
+//     }
+//
+// }
+//
+// userInfo();
+
