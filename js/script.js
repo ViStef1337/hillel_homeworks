9dfc70337aa31e9eda0471fd99933da781683e4a
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-10-05 11:23:21 -0500

    hw_5 version2

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 'use strict'
 // Знайти суму всіх цілих чисел від 1 до 15
-let sum = 0;
+let sum1 = 0;
 
 for (let i = 1; i <= 15; i++) {
-    sum += i;
+    sum1 += i;
 }
 
-console.log(sum);
+console.log(sum1);
 // Знайти добуток усіх цілих чисел від 15 до 35
-let sum = 1;
+let sum2 = 1;
 
 for (let i = 15; i <= 35; i++) {
-    sum *= i;
+    sum2 *= i;
 }
 
-console.log(sum);
+console.log(sum2);
 // Знайти середнє арифметичне всіх цілих чисел від 1 до 500
-let sum = 0
+let sum3 = 0
 let avg
 
 for (let i = 1; i <= 500; i++) {
-    sum += i;
-    avg = sum / 500
+    sum3 += i;
+    avg = sum3 / 500
 }
 
 console.log(avg);
 // Вивести суму лише парних чисел у діапазоні від 30 до 80
 
-let sum = 0
+let sum4 = 0
 for (let i = 30; i <= 80; i++) {
     if(i % 2 === 0){
-        sum += i
+        sum4 += i
     }
 }
-console.log(sum)
+console.log(sum4)
 
 // Вивести на консоль таблицю Піфагора з використанням циклів.
 for (let i = 1; i <= 10; i++) {
