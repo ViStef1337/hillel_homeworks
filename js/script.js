b28822d9757c7869bd3ac918743f796247218b6c
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-10-16 04:51:42 -0500

    hw_7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 'use strict'
+function createSumFunction() {
+    let total = 0;
+
+    function sum(num) {
+        total += num;
+        return total;
+    }
+
+    return sum;
+}
+
+const sum = createSumFunction();
+
+console.log(sum(4));  // 4
+console.log(sum(6));  // 10
+console.log(sum(10)); // 20
+console.log(sum(7));  // 27
+
+
+
+
+function multiplication (x){
+    return function (y){
+        return x * y;
+    }
+}
+
+
+console.log(multiplication(2)(5));
+
+function cycle(){
+    for(let i = 0; i<10 ;i++){
+        let num = prompt(`ваше число , ${i+1}`)
+        if (num > 100){
+            console.log('Користувач ввів число більше 100:', num);
+            return;
+        }else {
+            alert('введіть число ще раз')
+        }
+    }
+    console.log('досягнуто максимальну кількість ітерацій.');
+}
+cycle()
+
+
+
