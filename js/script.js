428871b4a00270154ee449b8142ee8a3abefa712
W1tcher <mudakoff322@gmail.com>
2023-11-04 01:39:01 -0500

    hw hw

--- js/script.js
+++ js/script.js
@@ -138,7 +138,7 @@
 // console.log(newString)
 
 //splice slice 🍎🍎🍎🍎
-//якшо ключ находиться в зсінній до обєкта звертаємось через квадратні душки
+//якшо ключ находиться в перемнній до обєкта звертаємось через квадратні душки
 //object keys object values
 //ключами в обєкті можуть бути тільки рядки
 //диструктаризація
@@ -156,11 +156,29 @@
 // console.log(typeof {})
 // console.log(Array.isArray({}))
 
-const salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130,
-};
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+//
+// const {John: asdasdasd , Ann , Pete} = salaries
+// console.log(asdasdasd,Ann,Pete)
+//
+//
+// const arr = [1,2,3,4,5]
+//
+// const [asdasd , second ,,,five] = arr
+// console.log(asdasd,second,five)
+
+
+// console.log('A'.charCodeAt(0))
+// console.log('Z'.charCodeAt(0))
+// console.log('a'.charCodeAt(0))
+// console.log('z'.charCodeAt(0))
+
+
+
 // const allSalaries = Object.values(salaries)
 // console.log(allSalaries)
 // let total = 0
@@ -193,81 +211,81 @@ const salaries = {
 
 
 
-const filmoteka = [
-  {
-    id: 1,
-    title: 'Duna',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    rating: 10,
-    actors: [
-      'Amelia Gibson',
-      'Cameron Clayton',
-      'Viola Vasquez',
-      'Lula Collins',
-      'Victor Nichols',
-    ],
-    adult: false,
-    image: 'https://via.placeholder.com/640/480',
-  },
-  {
-    id: 2,
-    title: 'Venom',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    rating: 8,
-    actors: [
-      'Inez Greer',
-      'Barry Curtis',
-      'Blanche Rios',
-      'Glen Perry',
-      'Logan Powers',
-    ],
-    adult: true,
-    image: 'https://via.placeholder.com/640/480',
-  },
-  {
-    id: 3,
-    title: 'Luca',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    rating: 9,
-    actors: [
-      'Marguerite Gibbs',
-      'Lora Alvarez',
-      'Jorge Simpson',
-      'Thomas Hall',
-      'Darrell Hunter',
-    ],
-    adult: false,
-  },
-  {
-    id: 4,
-    title: 'Free Guy',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    rating: 6,
-    actors: [
-      'Calvin Andrews',
-      'Mamie Myers',
-      'Madge Townsend',
-      'Ralph Kim',
-      'Jorge Reese',
-    ],
-    adult: false,
-    image: 'https://via.placeholder.com/640/480',
-  },
-  {
-    id: 5,
-    title:  'Saw',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    rating: 7,
-    actors: [
-      'Adele Marsh',
-      'Melvin Burgess',
-      'Jesus Reese',
-      'Harriet Moreno',
-      'Curtis Cox',
-    ],
-    adult: true,
-  },
-];
+// const filmoteka = [
+//   {
+//     id: 1,
+//     title: 'Duna',
+//     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     rating: 10,
+//     actors: [
+//       'Amelia Gibson',
+//       'Cameron Clayton',
+//       'Viola Vasquez',
+//       'Lula Collins',
+//       'Victor Nichols',
+//     ],
+//     adult: false,
+//     image: 'https://via.placeholder.com/640/480',
+//   },
+//   {
+//     id: 2,
+//     title: 'Venom',
+//     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     rating: 8,
+//     actors: [
+//       'Inez Greer',
+//       'Barry Curtis',
+//       'Blanche Rios',
+//       'Glen Perry',
+//       'Logan Powers',
+//     ],
+//     adult: true,
+//     image: 'https://via.placeholder.com/640/480',
+//   },
+//   {
+//     id: 3,
+//     title: 'Luca',
+//     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     rating: 9,
+//     actors: [
+//       'Marguerite Gibbs',
+//       'Lora Alvarez',
+//       'Jorge Simpson',
+//       'Thomas Hall',
+//       'Darrell Hunter',
+//     ],
+//     adult: false,
+//   },
+//   {
+//     id: 4,
+//     title: 'Free Guy',
+//     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     rating: 6,
+//     actors: [
+//       'Calvin Andrews',
+//       'Mamie Myers',
+//       'Madge Townsend',
+//       'Ralph Kim',
+//       'Jorge Reese',
+//     ],
+//     adult: false,
+//     image: 'https://via.placeholder.com/640/480',
+//   },
+//   {
+//     id: 5,
+//     title:  'Saw',
+//     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     rating: 7,
+//     actors: [
+//       'Adele Marsh',
+//       'Melvin Burgess',
+//       'Jesus Reese',
+//       'Harriet Moreno',
+//       'Curtis Cox',
+//     ],
+//     adult: true,
+//   },
+// ];
 
 // function getAllTitlesOfFilms(films){
 //   const allTitles = []
@@ -294,25 +312,121 @@ const filmoteka = [
 
 
 //? Напишіть функцію getAdultFilms(films), яка повертає масив усіх дорослих фільмів.
-function getAdultFilms(films) {
-  const arrAdultFilms = []
-  for(const film of films){
-    if (film.adult){
-      arrAdultFilms.push(film)
-    }
-  }
-  return arrAdultFilms;
-}
-console.log(getAdultFilms(filmoteka))
+// function getAdultFilms(films) {
+//   const arrAdultFilms = []
+//   for(const film of films){
+//     if (film.adult){
+//       arrAdultFilms.push(film)
+//     }
+//   }
+//   return arrAdultFilms;
+// }
+// console.log(getAdultFilms(filmoteka))
 
 /*
 ? Напишіть ф-цію calcTotalPrice(stones, stoneName), яка приймає масив об'єктів та рядок під назвою каменю.
 ? Ф-ція рахує і повертає загальну вартість каміння з таким ім'ям.
 */
+//
+// const stones = [
+//   { name: 'Смарагд', price: 1300, quantity: 4 },
+//   { name: 'Діамант', price: 2700, quantity: 3 },
+//   { name: 'Сапфір', price: 400, quantity: 7 },
+//   { name: 'Щебінь', price: 200, quantity: 2 },
+// ];
+//
+// function calcTotalPrice(stones, stoneName){
+//   let totalPrice = 0
+//   for(let stone of stones){
+//     if (stone.name === stoneName){
+//       totalPrice += stone.price * stone.quantity
+//     }
+//   }
+//   return totalPrice
+// }
+// console.log(calcTotalPrice(stones, 'Смарагд'))
+//
+//
+//
+//
+//
+//
+// const numbers = [1, 2, 3, 4, 5];
+// const sum = numbers.reduce(function (accumulator, number) {
+//   return accumulator + number;
+// }, 0);
+// console.log(sum);
 
-const stones = [
-  { name: 'Смарагд', price: 1300, quantity: 4 },
-  { name: 'Діамант', price: 2700, quantity: 3 },
-  { name: 'Сапфір', price: 400, quantity: 7 },
-  { name: 'Щебінь', price: 200, quantity: 2 },
-];
+// const fruits = ["🍎", "🍇", "🍑", "🍌", "🍋"]
+//
+// const newArr = fruits.unshift("🍋")
+// fruits.push("🍇")
+// console.log(fruits)
+
+
+// const vehicles = [
+//   { make: 'Honda', model: 'CR-V', type: 'suv', amount: 14, price: 24045, onSale: true },
+//   { make: 'Honda', model: 'Accord', type: 'sedan', amount: 2, price: 22455, onSale: true },
+//   { make: 'Mazda', model: 'Mazda 6', type: 'sedan', amount: 8, price: 24195, onSale: false },
+//   { make: 'Mazda', model: 'CX-9', type: 'suv', amount: 7, price: 31520, onSale: true },
+//   { make: 'Toyota', model: '4Runner', type: 'suv', amount: 19, price: 34210, onSale: false },
+//   { make: 'Toyota', model: 'Sequoia', type: 'suv', amount: 16, price: 45560, onSale: false },
+//   { make: 'Toyota', model: 'Tacoma', type: 'truck', amount: 4, price: 24320, onSale: true },
+//   { make: 'Ford', model: 'F-150', type: 'truck', amount: 11, price: 27110, onSale: true },
+//   { make: 'Ford', model: 'Fusion', type: 'sedan', amount: 13, price: 22120, onSale: true },
+//   { make: 'Ford', model: 'Explorer', type: 'suv', amount: 6, price: 31660, onSale: false },
+// ];
+//
+//
+// function calculateVehicles(vehicles,model){
+//   let total = 0
+//   for(const car of vehicles){
+//     if(car.model === model){
+//       total += car.price * car.amount
+//     }
+//   }
+//   return total
+// }
+// console.log(calculateVehicles(vehicles,'CX-9'))
+
+
+
+// function friend(friends){
+//   const myFriends = []
+//   for(const name of friends){
+//     if (name.length===4){
+//       myFriends.push(name)
+//     }
+//   }
+//   return myFriends
+// }
+//
+// console.log(friend(["Ryan", "Kieran", "Mark"]))
+
+
+// function findNextSquare(sq) {
+//   const number = Math.sqrt(sq)
+//   const isInteger = Number.isInteger(number)
+//   if(isInteger){
+//     return Math.pow(number+1,2)
+//   }
+//   return -1;
+// }
+// console.log(findNextSquare(114))
+
+
+function getName(name,callback){
+  callback(name)
+}
+
+
+getName('mango',function showName(name){
+  console.log(name)
+})
+
+
+//5 задач з код варс
+// стрілочні функції
+// пере бираючі методи масивів forEach() map() filter() приклади використання
+// що повертає які параметри оголошує колбек функція для чого використовуєтсья
+const numbers = [1,2,3,4,5]
