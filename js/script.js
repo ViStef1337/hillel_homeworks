6c3cd8fe08ea6f42c61906cc3d8c1b109bc25a37
Vistef <stefanykvitalik1337@gmail.com>
2023-11-15 21:46:37 -0600

    fsddf

--- js/script.js
+++ js/script.js
@@ -415,18 +415,408 @@
 // console.log(findNextSquare(114))
 
 
-function getName(name,callback){
-  callback(name)
-}
-
-
-getName('mango',function showName(name){
-  console.log(name)
-})
+// function getName(name,callback){
+//   callback(name)
+// }
+//
+//
+// getName('mango',function showName(name){
+//   console.log(name)
+// })
 
 
 //5 задач з код варс
 // стрілочні функції
 // пере бираючі методи масивів forEach() map() filter() приклади використання
 // що повертає які параметри оголошує колбек функція для чого використовуєтсья
-const numbers = [1,2,3,4,5]
+// const numbers = [1,2,3,4,5]
+// spread rest
+
+// const year = 2000
+// function isLeapYear(year) {
+//
+//     if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+//         return true;
+//     }
+//
+//   return false;
+// }
+//
+// console.log(isLeapYear(year))
+
+// function validatePIN(pin) {
+//   if (pin.length === 4 || pin.length === 6) {
+//     return !isNaN(pin);
+//   }
+//   return false;
+// }
+//
+// console.log(validatePIN("1234"));
+// console.log(validatePIN("12345"));
+// console.log(validatePIN("123456"));
+// console.log(validatePIN("a1b2c3"));
+//
+// console.log(!isNaN('21'))
+
+// function descendingOrder(n){
+//   let reversed = ''
+//   for(let i = 0; i<n.length; i++){
+//     reversed += n[n.length-1-i]
+//   }
+//   return parseInt(reversed)
+// }
+// console.log(descendingOrder(12345))
+
+
+
+//'10'>'2'
+// console.log([1,2,3,100].sort((a, b)=>a-b))
+// function descendingOrder(n){
+//   const arrNumbers = n.toString().split('')
+//   const sortedNumbers = arrNumbers.sort((a, b)=>b-a)
+//   console.log(sortedNumbers.join(''))
+//   return Number(sortedNumbers.join(''))
+// }
+//
+// console.log(descendingOrder(129345))
+
+
+// function sumTwoSmallestNumbers(numbers) {
+//   let total = ''
+//   for(let i=0;i<numbers.length;i++){
+//     if(number[i]){
+//
+//     }
+//   }
+// }
+
+
+// const numbers = [1, 2, 3, 4, 5];
+//
+// numbers.forEach(function(number) {
+//   console.log(number);
+// });
+
+// const numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7]
+//
+// const filteredArr = []
+//
+// for(let i=0; i<numbers.length;i++){
+//   if(i === numbers.indexOf(numbers[i])){
+//     filteredArr.push(numbers[i])
+//   }
+// }
+// console.log(filteredArr)
+
+
+// function countSheeps(sheep) {
+//   let count = 0;
+//   for(let i=0;i<sheep.length;i++){
+//     if(sheep[i]===true){
+//       count+=1
+//     }
+//   }
+//   return count
+// }
+//
+//
+//
+// console.log(countSheeps([true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]))
+
+
+// function toUpperCase (string) {
+//   let newString = ''
+//   for(let i = 0; i<this.length;i++){
+//     if(this[i]===this[i].toUpperCase()){
+//       newString += this[i].toLowerCase()
+//     } else {
+//       newString += this[i].toUpperCase()
+//     }
+//   }
+//   return newString;
+// }
+//
+// console.log(toUpperCase("HeLLo WoRLD"))
+
+
+
+  // function findSmallestInt(args) {
+  //   let theSmallestNumber = args[0]
+  //   for (let i = 0; i<args.length;i++){
+  //     if (args[i]<theSmallestNumber){
+  //       theSmallestNumber = args[i]
+  //     }
+  //   }
+  //   return theSmallestNumber
+  // }
+  //
+  // console.log(findSmallestInt([34, 15, 88, 2]))
+
+// const numbers = [1,2,3]
+// console.log(Math.min(...numbers))
+
+
+
+// function evenOrOdd(number) {
+//     if(number % 2 === 0){
+//         return "Even"
+//     } else{
+//         return "Odd"
+//     }
+// }
+//
+// console.log(evenOrOdd(5))
+
+// function basicOp(operation, value1, value2){
+//     if (operation === '+'){
+//         let plus = value1+value2
+//         return plus
+//     }else if (operation === '-'){
+//         let minus = value1-value2
+//         return minus
+//     }else if (operation === '/'){
+//         let divide = value1/value2
+//         return divide
+//     }else if (operation === '*'){
+//         let multiplication = value1*value2
+//         return multiplication
+//     }
+// }
+
+
+// function litres(time) {
+//     let water = time * 0.5
+//     return Math.floor(water)
+// }
+
+
+// const arr = [1,2,3,4,5]
+//
+// function sum (...arr){
+//
+//     console.log(arr)
+// }
+// sum (1, 2,3,4,5)
+
+
+// function lovefunc(flower1, flower2){
+//         return flower1 % 2 !== flower2 % 2
+// }
+//
+// console.log(lovefunc(1, 4))
+
+
+// function squareOrSquareRoot(array) {
+//
+//     return array.map((element)=>{
+//         if(Number.isInteger(Math.sqrt(element))){
+//            return Math.sqrt(element)
+//         }else {
+//             return Math.pow(element,2)
+//         }
+//     });
+// }
+
+
+// function squareOrSquareRoot(array) {
+//
+//     return array.map((element)=>{
+//         return Number.isInteger(Math.sqrt(element)) ? Math.sqrt(element) : Math.pow(element,2)
+//     });
+// }
+// console.log(squareOrSquareRoot([4,3,9,7,2,1]))
+
+
+// function mergeArrays(a, b) {
+//     return [...a,...b].filter((element,index,array)=>index===array.indexOf(element)).sort((a,b)=>a-b)
+// }
+// function mergeArrays(a, b) {
+//     let c = [...a,...b]
+//     let filtered = []
+//     for (let i=0;i<c.length;i++){
+//         if(i === c.indexOf(c[i])){
+//             filtered.push(c[i])
+//         }
+//     }
+//     return filtered.sort((a,b)=>a-b)
+// }
+
+
+// console.log(mergeArrays([1, 3, 5], [2, 4, 6]))
+
+
+// const arr = [1,2,3]
+// const arr2 = [4,5]
+//
+// console.log([...arr,...arr2])
+
+
+
+
+
+// function makeNegative(num) {
+//     return num <= 0 ? num : -num
+// }
+
+
+// function isUpperCase (string) {
+//     for(let i =0;i<string.length;i++){
+//         if(string[i] !== string[i].toUpperCase()){
+//             return false
+//         }
+//     }
+//     return true
+// }
+//
+// function isUpperCase (string) {
+//     return string == string.toUpperCase()
+// }
+//
+// console.log(isUpperCase ("ACSKLDFJSGSKLDFJSKLDFJ"))
+
+
+// function hero(bullets, dragons){
+//     if(bullets >= dragons*2){
+//         return true
+//     } else return false
+// }
+//
+// console.log(hero(100, 40))
+
+
+// function greet(name){
+//     return `Hello, ${name} how are you doing today?`;
+// }
+//
+// console.log(greet('Vitaliy'))
+
+// function whoseMove(lastPlayer, win) {
+//     if(lastPlayer === "black" && win === true){
+//         return "black"
+//     } else if(lastPlayer === "black" && win === false){
+//         return "white"
+//     } else if(lastPlayer === "white" && win === false) {
+//         return "black"
+//     }else if(lastPlayer === "white" && win === true) {
+//         return "white"
+//     }
+// }
+
+
+// function whoseMove(lastPlayer, win) {
+//     return win?lastPlayer:lastPlayer=="white"?"black":"white"
+// }
+
+
+// function feast(beast, dish) {
+//     if(beast[0] === dish[0] && beast[beast.length-1]===dish[dish.length-1]){
+//         return true
+//     }else return false
+// }
+
+
+
+// function max (list){
+//     let theBigest = list[0]
+//     for(let i=0;i<list.length;i++){
+//         if(theBigest<list[i]){
+//             theBigest = list[i]
+//         }
+//     }
+//     return theBigest
+// }
+//
+// console.log(max ([42, 54, 65, 87, 0]))
+//
+// function min (list){
+//     let theSmallest = list[0]
+//     for(let i=0;i<list.length;i++){
+//         if(theSmallest>list[i]){
+//             theSmallest = list[i]
+//         }
+//     }
+//     return theSmallest
+// }
+//
+// console.log(min ([42, 54, 65, 87, 0]))
+
+
+// const addArrow = (a, b) => ({a , b});
+//
+//
+// console.log(addArrow(2,3))
+
+// const arr = [1,2,3,4].map((element)=>{return element *2})
+// console.log(arr)
+
+// const numbers = [1,1,2,2,3,3,4,4]
+// const filteredArr = []
+// for(let i =0;i<numbers.length;i++){
+//     if(i===numbers.indexOf(numbers[i])){
+//         filteredArr.push(numbers[i])
+//     }
+// }
+// console.log(filteredArr)
+// const filteredArr = numbers.filter((element,index,arr)=>arr.indexOf(element)===index)
+
+// const str = 'java script це цікаво'
+// const reversedStr = str.split('').reverse().join('')
+// const arr = []
+// str.split('').forEach((element)=>{arr.unshift(element)})
+// for(let i=0;i<str.length;i++){
+//     console.log(str[str.length-1-i])
+// }
+//
+// console.log(arr.join(''))
+
+
+// function describeAge(age) {
+//     if (age <= 12) {
+//         return "You're a(n) kid";
+//     } else if (age >= 13 && age <= 17) {
+//         return "You're a(n) teenager";
+//     } else if (age >= 18 && age <= 64) {
+//         return "You're a(n) adult";
+//     } else {
+//         return "You're a(n) elderly";
+//     }
+//     // return age<=12?"You're a(n) kid":age<=17?"You're a(n) teenager":age<=64?"You're a(n) adult":"You're a(n) elderly"
+//     return `You're a(n) ${age<=12?"kid":age<=17?"teenager":age<=64?"adult":"elderly"}`
+// }
+
+
+// function removeChar(str){
+//     let emptyStr = ''
+//     for(let i = 1;i<str.length-1;i++){
+//         emptyStr+=str[i]
+//     }
+//     return emptyStr
+// };
+//
+// function removeChar(str) {
+//     return str.slice(1, -1);
+// }
+
+
+// function abbrevName(name){
+//     const arr = name.split(' ')
+//     return `${arr[0][0]}.${arr[1][0]}`
+// }
+//
+// console.log(abbrevName('Sam Harris'))
+
+// const name = 'mango'
+// console.log({name})
+
+function isDigit(s) {
+    return Number(s)
+}
+
+// console.log(isDigit('-234.4f'))
+
+console.log(NaN===NaN)
