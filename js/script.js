332a34353308799cc74b657c7a72f4632602da60
Vistef <stefanykvitalik1337@gmail.com>
2024-01-26 22:01:15 -0600

    uiu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 'use strict'
+const menu = document.querySelector('.acardion-list')
+menu.addEventListener('click',(e)=>{
+    if (e.target.nodeName!=='BUTTON')return;
+    const panel = e.target.nextElementSibling
+    panel.classList.toggle('active')
+})
