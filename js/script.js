7deb3ea514048614e06cc4cf5979efc2b7d4ea75
Vistef <stefanykvitalik1337@gmail.com>
2024-01-29 12:30:00 -0600

    iii

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict'
+const button = document.querySelector('.open-model')
+const backDrop = document.querySelector('.back-drop')
+const close = document.querySelector('.close')
+
+const  toggleModel = () => {
+    backDrop.classList.toggle('is-hidden')
+}
+
+
+button.addEventListener('click',toggleModel)
+close.addEventListener('click',toggleModel)
