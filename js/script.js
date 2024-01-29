17d55cfa5b301046fd66a6b10632a413048dcb1b
Vistef <stefanykvitalik1337@gmail.com>
2024-01-29 12:49:20 -0600

    ooo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict'
+const pictures = [
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/0/700/460',
+        alt: 'laptop',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/1/700/460',
+        alt: 'developer',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/10/700/460',
+        alt: 'forest',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/100/700/460',
+        alt: 'beach',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/1000/700/460',
+        alt: 'mountain',
+    },
+];
+const gallery = document.querySelector('.gallery')
+const galleryList = pictures.map(item=>`<li><img src="${item.url}" alt="item.alt"></li>`).join('')
+gallery.insertAdjacentHTML('beforeend',galleryList)
+
+//при клыцы на зоображення щоб выдкривалось выкно з цим зоображенням
