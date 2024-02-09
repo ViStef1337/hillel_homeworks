055847b4d941fe393fe6a4e42d393d1781f72330
Vistef <stefanykvitalik1337@gmail.com>
2024-02-09 11:43:22 -0600

    yutyu

--- js/script.js
+++ js/script.js
@@ -10,3 +10,49 @@ const  toggleModel = () => {
 
 button.addEventListener('click',toggleModel)
 close.addEventListener('click',toggleModel)
+
+
+const pictures = [
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/0/700/460',
+        alt: 'laptop',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/1/700/460',
+        alt: 'developer',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/10/700/460',
+        alt: 'forest',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/100/700/460',
+        alt: 'beach',
+    },
+    {
+        width: 700,
+        height: 460,
+        url: 'https://picsum.photos/id/1000/700/460',
+        alt: 'mountain',
+    },
+];
+const gallery = document.querySelector('.gallery')
+const galleryList = pictures.map(item=>`<li><img src="${item.url}" class="picture" data-url="${item.url}" alt="item.alt"></li>`)
+gallery.insertAdjacentHTML('beforeend',galleryList)
+
+const images = document.querySelectorAll('.picture')
+images.forEach(image=>{
+    image.addEventListener('click',(e)=>{
+        const modalImg = backDrop.querySelector('img')
+        modalImg.src=e.target.dataset.url
+        toggleModel()
+    })
+})
