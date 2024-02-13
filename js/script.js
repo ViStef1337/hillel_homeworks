82bc19f474a10cb05619ce1c6bd4591645343924
Vistef <stefanykvitalik1337@gmail.com>
2024-02-13 12:25:26 -0600

    uyi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const list = document.querySelector('.js-pallet')
+
+list.childNodes
+
+console.log(list.childNodes)
+
+function getRandomHex() {
+    return Math.round(Math.random() * 256)
+        .toString(16)
+        .padStart(2, '0');
+}
