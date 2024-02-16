e67b93f2360b4c52a398df505dab79b53559a947
Vistef <stefanykvitalik1337@gmail.com>
2024-02-16 12:32:44 -0600

    ppp

--- js/script.js
+++ js/script.js
@@ -4,24 +4,63 @@ const list = document.querySelector('.js-pallet')
 
 const liArray = [...list.children]
 liArray.forEach(item=>{
-    item.style.backgroundColor = getRangomColor()
+    const color = getRangomColor()
+    item.style.backgroundColor = color
+    item.dataset.color = color
+    // item.firstElementChild.textContent= color
 })
+
+
+
+
 
 list.addEventListener('click',(e)=>{
     if (e.target.nodeName==='UL'){
         return
     }
+
+    console.log(e.target.parentNode)
     const isActive = list.querySelector('.active')
 
     if (isActive){
         isActive.classList.remove('active')
+        isActive.firstElementChild.textContent='...'
     }
-    if (e.target===isActive){
+
+    if (e.target===isActive || e.target.parentNode===isActive){
         return;
     }
+
+    if (e.target.nodeName==='SPAN'){
+        e.target.parentNode.classList.add('active')
+        e.target.textContent = e.target.parentNode.dataset.color
+        return;
+    }
+
     e.target.classList.add('active')
-    console.log(isActive)
+
+    e.target.firstElementChild.textContent=e.target.dataset.color
 })
+
+// list.addEventListener('click',(e)=>{
+//     if (e.target.nodeName==='UL'){
+//         return
+//     }
+//     const item = e.target.closest('.pallet__item')
+//     console.log(item)
+//     const isActive = list.querySelector('.active')
+//
+//     if (isActive){
+//         isActive.classList.remove('active')
+//         isActive.firstElementChild.textContent='...'
+//     }
+//     if (item===isActive){
+//         return;
+//     }
+//     item.classList.add('active')
+//     item.firstElementChild.textContent=item.dataset.color
+// })
+
 
 // console.log(list.childNodes)
 
