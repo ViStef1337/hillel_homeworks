b7d872fcab979438ecc9d05ae4dd161e3fe9d971
Vistef <stefanykvitalik1337@gmail.com>
2024-03-07 16:46:24 -0600

    slaider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+
+new Swiper('.image-slider',{
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        renderBullet: function (index, className){
+            return '<span class="'+className+'">'+(index+1)+'</span>'
+        }
+    }
+})
+
