717bf189240f2745fdf393d9273e5b214c26172f
Vistef <stefanykvitalik1337@gmail.com>
2024-03-21 20:37:14 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict'
+const ul = document.querySelector('.js-key__list')
+ul.addEventListener('transitionend',(e)=>{
+    if (e.propertyName==='transform'){
+        e.target.classList.remove('playing')
+    }
+})
+ul.addEventListener('click',(e)=>{
+    if (e.target.nodeName === 'UL'){
+        return
+    }
+    const li = e.target.closest('.key__item')
+    li.classList.add('playing')
+    soundPlay(li.dataset.key)
+})
+
+window.addEventListener('keydown',(e)=>{
+    const li = document.querySelector(`li[data-key='${e.keyCode}']`)
+    if (!li){
+        return
+    }
+    li.classList.add('playing')
+    soundPlay(e.keyCode)
+})
+
+function soundPlay (key){
+    const sound = document.querySelector(`audio[data-key='${key}']`)
+    sound.play()
+}
