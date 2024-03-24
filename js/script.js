9f053fb152e48480adcce5dbe494a8e47ede0587
Vistef <stefanykvitalik1337@gmail.com>
2024-03-23 23:32:30 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict'
+
+let link = document.getElementsByClassName('link')
+
+let currentValue = 1
+
+function activeLink(event) {
+    // Отримуємо всі елементи, які мають клас 'link'
+    const link = document.querySelectorAll('.link');
+
+    // Видаляємо клас 'active' у всіх елементів з класом 'link'
+    link.forEach(l => {
+        l.classList.remove('active');
+    });
+
+    // Додаємо клас 'active' елементу, на який клікнули
+    event.target.classList.add('active');
+
+    // Присвоюємо значення 'currentValue' властивості 'value' елементу, на який клікнули
+    currentValue = event.target.value;
+}
