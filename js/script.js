f16359f65d30fca6713e7bfcd6cf3419199362a0
Vistef <stefanykvitalik1337@gmail.com>
2024-03-31 11:42:24 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict'
-const ulTag = document.querySelector('ul')
-let totalPages = 21
+// const ulTag = document.querySelector('ul')
+// let totalPages = 21
 
 function element (totalPages,page){
     let liTag = ''
@@ -67,9 +67,6 @@ element(totalPages,1) //calling function with passing values
 
 
 
-export function qwe(){
-    console.log('hello')
-}
 
 
 
@@ -103,17 +100,18 @@ export function qwe(){
 // const ulTag = document.querySelector('ul')
 // let totalPages = 20
 //
-// function element (totalPages,page){
+// function createPagination (totalPages,page){
 //     let liTag = ''
 //     let activeLi;
 //     let beforePages = page - 1 //5 - 1 = 4
 //     let afterPages = page + 1 //5 + 1 = 6
+//
 //     if (page > 1){//if page value is grater than 1 then add new li which is previous button
-//         liTag += `<li class="btn prev" onclick="element(totalPages,${page - 1})"><span><i class="fa-solid fa-chevron-left"></i>Prev</span></li>`
+//         liTag += `<li class="btn prev" onclick="createPagination(totalPages,${page - 1})"><span><i class="fa-solid fa-chevron-left"></i>Prev</span></li>`
 //     }
 //
 //     if (page > 2){ //if page value grater than 2 then add new li tag with 1 value
-//         liTag += `<li class="numb" onclick="element (totalPages,1)"><span>1</span></li>`
+//         liTag += `<li class="numb" onclick="createPagination(totalPages,1)"><span>1</span></li>`
 //         if (page > 3){ //if page value is grater than 3 then add new li tag with (...)
 //             liTag += `<li class="dots"><span>...</span></li>`
 //         }
@@ -146,19 +144,19 @@ export function qwe(){
 //         }else { //else leave empty to the activeLi variable
 //             activeLi = ""
 //         }
-//         liTag += `<li class="numb ${activeLi}" onclick="element (totalPages,${pageLength})"><span>${pageLength}</span></li>`
+//         liTag += `<li class="numb ${activeLi}" onclick="createPagination(totalPages,${pageLength})"><span>${pageLength}</span></li>`
 //     }
 //
 //     if (page < totalPages - 1){ //if page value grater than totalPages by -1 then show the last li or page which is 20
 //         if (page < totalPages - 2){ //if page value grater than totalPages by -2 then show the last (...) before last page
 //             liTag += `<li class="dots"><span>...</span></li>`
 //         }
-//         liTag += `<li class="numb" onclick="element (totalPages,${totalPages})"><span>${totalPages}</span></li>`
+//         liTag += `<li class="numb" onclick="createPagination(totalPages,${totalPages})"><span>${totalPages}</span></li>`
 //     }
 //
 //     if (page < totalPages){ //if page value is less than totalPages value then add new li which is next button
-//         liTag += `<li class="btn next" onclick="element(totalPages,${page + 1})"><span>Next<i class="fa-solid fa-chevron-right"></i></span></li>`
+//         liTag += `<li class="btn next" onclick="createPagination(totalPages,${page + 1})"><span>Next<i class="fa-solid fa-chevron-right"></i></span></li>`
 //     }
 //     ulTag.innerHTML = liTag
 // }
-// element(totalPages,5) //calling function with passing values
+// createPagination(totalPages,5) //calling function with passing values
