772a0ee535ce1f0d02c2f3cae568b8801432dcd0
Vistef <stefanykvitalik1337@gmail.com>
2024-04-03 18:20:31 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict'
-// const ulTag = document.querySelector('ul')
-// let totalPages = 21
+const ulTag = document.querySelector('ul')
+let totalPages = 5
 
 function element (totalPages,page){
     let liTag = ''
@@ -11,29 +11,7 @@ function element (totalPages,page){
         liTag += `<li class="btn prev" onclick="element(totalPages,${page - 1})"><span><i class="fa-solid fa-chevron-left"></i>Prev</span></li>`
     }
 
-    // if (page > 2){ //if page value grater than 2 then add new li tag with 1 value
-    //     liTag += `<li class="numb" onclick="element (totalPages,1)"><span>1</span></li>`
-    //     if (page > 3){ //if page value is grater than 3 then add new li tag with (...)
-    //         liTag += `<li class="dots"><span>...</span></li>`
-    //     }
-    // }
-
-    //how many pages or li show before the current li
-    // if (page == totalPages){ //if page value os equal to totalPages the substract by -2 to the beforePages value
-    //     beforePages = beforePages - 2
-    // }else if (page == totalPages - 1){ //else if page value os equal to totalPages by -1 the substract by -1 to the beforePages value
-    //     beforePages = beforePages -1
-    // }
-
-    //how many pages or li show after the current li
-    // if (page == 1){//if page value os equal to 1 then add by +2 to the afterPages value
-    //     afterPages = afterPages + 2
-    // }else if (page == 2){ //else if page value os equal to 2 then add by +1 to the afterPages value
-    //     beforePages = beforePages + 1
-    // }
-
-
-    for (let pageLength = beforePages; pageLength <= afterPages; pageLength++){
+    for (let pageLength = 1; pageLength <= 5; pageLength++){
         if (pageLength > totalPages){
             continue
         }
@@ -47,13 +25,6 @@ function element (totalPages,page){
         }
         liTag += `<li class="numb ${activeLi}" onclick="element (totalPages,${pageLength})"><span>${pageLength}</span></li>`
     }
-
-    // if (page < totalPages - 1){ //if page value grater than totalPages by -1 then show the last li or page which is 20
-    //     if (page < totalPages - 2){ //if page value grater than totalPages by -2 then show the last (...) before last page
-    //         liTag += `<li class="dots"><span>...</span></li>`
-    //     }
-    //     liTag += `<li class="numb" onclick="element (totalPages,${totalPages})"><span>${totalPages}</span></li>`
-    // }
 
     if (page < totalPages){ //if page value is less than totalPages value then add new li which is next button
         liTag += `<li class="btn next" onclick="element(totalPages,${page + 1})"><span>Next<i class="fa-solid fa-chevron-right"></i></span></li>`
