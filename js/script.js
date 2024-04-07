075e75a5a5ae1c3434e14de08f4bf21284cf8cb8
Vistef <stefanykvitalik1337@gmail.com>
2024-04-06 19:34:43 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict'
+const form = document.querySelector('form')
+const data = {}
+
+const LOCAL_STORAGE_KEY = 'form_value'
+
+form.addEventListener('input',(e)=>{
+    data[e.target.name]=e.target.value
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(data))
+})
+
+function loadPage (){
+    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const parsedData = JSON.parse(storedData)
+    form.elements.message.value=parsedData.message
+    form.elements.email.value=parsedData.email
+    console.log(form.elements.message)
+    console.log(parsedData)
+}
+
+loadPage()
