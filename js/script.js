e2f9765b37623a4cc4bb8cf6ba1144ba059d4c6b
Vistef <stefanykvitalik1337@gmail.com>
2024-04-10 18:01:03 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
 'use strict'
 const ul = document.querySelector('.js-key__list')
+
+
+ul.addEventListener('click',(e)=>{
+    if (e.target.nodeName==='UL'){
+        return
+    }
+    const li = e.target.closest('.key__item')
+    li.classList.add('playing')
+    console.log(li.dataset.key)
+    const audio = document.querySelector(`audio[data-key="${li.dataset.key}"]`)
+    audio.play()
+})
 ul.addEventListener('transitionend',(e)=>{
     if (e.propertyName==='transform'){
         e.target.classList.remove('playing')
     }
 })
-ul.addEventListener('click',(e)=>{
-    if (e.target.nodeName === 'UL'){
-        return
-    }
-    const li = e.target.closest('.key__item')
-    li.classList.add('playing')
-    soundPlay(li.dataset.key)
-})
-
-window.addEventListener('keydown',(e)=>{
-    const li = document.querySelector(`li[data-key='${e.keyCode}']`)
-    if (!li){
-        return
-    }
-    li.classList.add('playing')
-    soundPlay(e.keyCode)
-})
-
-function soundPlay (key){
-    const sound = document.querySelector(`audio[data-key='${key}']`)
-    sound.play()
-}
