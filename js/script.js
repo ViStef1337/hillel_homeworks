c997b12f48f425de55d46e74fc15f8efdc429576
Vistef <stefanykvitalik1337@gmail.com>
2024-04-22 11:14:36 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict'
+const btnStart = document.querySelector('.start')
+
+const btnStop = document.querySelector('.stop')
+
+btnStart.addEventListener('click',()=>{
+    function changeBackGround() {
+        document.body.style.background
+    }
+})
+
+btnStop.addEventListener('click',()=>{
+
+})
