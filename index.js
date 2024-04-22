62092df468de2dcffd70851d39e4f9c480f2b395
Vistef <stefanykvitalik1337@gmail.com>
2024-04-22 11:04:09 -0500

    www

--- index.js
+++ index.js
@@ -13,32 +13,44 @@ ul.insertAdjacentHTML('beforeend',marcap)
 
 
 form.addEventListener('input',(e)=>{
-    const filteredCities = cities.filter(item=>item.city.includes(e.target.value)||item.state.includes(e.target.value))
+    const {value} = e.target
+    const filteredCities = cities.filter(item=>item.city.toLowerCase().includes(value.toLowerCase())||item.state.toLowerCase().includes(value.toLowerCase()))
     if (filteredCities.length===0){
-        const errorMessage = `<li style="color: red">nothing found with your query</li>>`
+        const errorMessage = `<li style="color: red">nothing found with your query</li>`
         ul.innerHTML=errorMessage
         return
     }
-    const marcap = createMarcap (filteredCities)
+    const marcap = createMarcap (filteredCities,value)
     ul.innerHTML=marcap
 })
 
-function createMarcap (arr){
-    return arr.map(item=>`<li><span class="name">${item.city},${item.state}</span><span class="population">${item.population}</span></li>`).join('')
+function createMarcap (arr,value){
+    return arr.map(item=>{
+        const cityName = value ? item.city.toLowerCase().replace(value.toLowerCase(),`<span class="hl">${value}</span>`) : item.city
+        const stateName = value ? item.state.toLowerCase().replace(value.toLowerCase(),`<span class="hl">${value}</span>`) : item.state
+        return `<li><span class="name">${cityName},${stateName}</span><span class="population">${item.population}</span></li>`
+    }).join('')
 }
 
+const wqe = 'New York'
 
+console.log(wqe.toLowerCase().replace('new','N'))
 
-// const users = [
-//     {
-//         name: 'Andriy'
-//     },
-//     {
-//         name: 'Vitaliy'
-//     },
-//     {
-//         name: 'Oleg'
-//     }
-// ]
-//
-// console.log(users.filter(item=>item.name === 'Oleg'))
+const btn = document.querySelector('.interval')
+
+btn.addEventListener('click',()=>{
+    // clearTimeout(timeOut)
+    console.log(timeout)
+})
+
+const interval = setInterval(()=>{
+    console.log('interval')
+},1000)
+console.log(interval)
+
+const timeout = 'timeout'
+
+// const timeOut = setTimeout(()=>{
+//     console.log('timeout')
+//     // clearInterval(interval)
+// },5000)
