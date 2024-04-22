13d42f978689f9e83a35a4bffff039f374da7df1
Vistef <stefanykvitalik1337@gmail.com>
2024-04-22 13:00:50 -0500

    www

--- js/script.js
+++ js/script.js
@@ -6,12 +6,46 @@ const btnStart = document.querySelector('.start')
 
 const btnStop = document.querySelector('.stop')
 
-const span = document.querySelector('span')
+const span = document.querySelector('.color')
+
+const input = document.querySelector('#myRange')
+
+const p = document.querySelector('.text')
+
+const hello = document.querySelector('.hello')
+
+const word = document.querySelector('.word')
 
 
-let interval = null
+hello.addEventListener('input',(e)=>{
+    if (!e.target.value){
+        word.textContent='world'
+        return
+    }
+    word.textContent=e.target.value
+})
 
-btnStop.disabled = true;
+// Отримуємо розмір шрифту
+// const fontSize = window.getComputedStyle(input).fontSize;
+//
+// const size = fontSize.slice(0,-2)
+//
+// input.value=size
+//
+// let interval = null
+//
+// btnStop.disabled = true;
+//
+// input.addEventListener('input',()=>{
+//
+// })
+
+p.style.fontSize=input.value+'px'
+
+input.addEventListener('input',()=>{
+    p.style.fontSize=input.value+'px'
+    console.log(input.value)
+})
 
 btnStart.addEventListener('click',()=>{
 
