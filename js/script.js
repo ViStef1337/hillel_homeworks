7ac45f4c7e8bf8f1dd62dab091e869eff35901d1
Vistef <stefanykvitalik1337@gmail.com>
2024-04-28 11:44:46 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 'use strict'
+
+const number = document.querySelector('.number');
+const increaseButton = document.querySelector('.increase');
+const decreaceButton = document.querySelector('.decrease');
+const resetButton = document.querySelector('.reset');
+
+let count = Number(localStorage.getItem('count')) || 0;
+
+updateColor (count)
+
+function updateColor (count){
+    number.textContent = count;
+    if (count>0){
+        number.style.color='green'
+    }else if (count<0){
+        number.style.color='red'
+    }else if (count===0){
+        number.style.color='black'
+    }
+    localStorage.setItem('count', count);
+}
+
+increaseButton.addEventListener('click', (e)=> {
+    count+=1;
+    updateColor (count)
+});
+
+
+decreaceButton.addEventListener('click', (e)=> {
+    count-=1;
+    updateColor (count)
+});
+
+resetButton.addEventListener('click', (e)=> {
+    count = 0;
+    updateColor (count)
+});
+
