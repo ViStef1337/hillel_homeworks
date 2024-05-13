6f6f3d9e129142d3e6d9f79b915ee7fc4feceb0d
Vistef <stefanykvitalik1337@gmail.com>
2024-05-13 17:55:15 -0500

    www

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict'
+const btnPrev = document.querySelector('#arrow-left')
+
+const btnNext = document.querySelector('#arrow-right')
+
+const slider = document.querySelector('.slider')
+
+const widthSlider = slider.scrollWidth
+
+const interval = 200
+let currentLeftPosition = parseInt(getComputedStyle(slider).left)
+console.log(currentLeftPosition)
+btnPrev.addEventListener('click',()=>{
+    let currentLeftPosition = parseInt(getComputedStyle(slider).left)
+    currentLeftPosition += interval
+    if (currentLeftPosition>0){
+        slider.style.left='0px'
+        return
+    }
+    slider.style.left=currentLeftPosition+'px'
+})
+
+btnNext.addEventListener('click',()=>{
+    let currentLeftPosition = parseInt(getComputedStyle(slider).left)
+    console.log(Math.abs(currentLeftPosition))
+    console.log(Math.abs(interval))
+    console.log(slider.clientWidth)
+    console.log(slider.scrollWidth)
+    if (Math.abs(currentLeftPosition)+interval+slider.clientWidth>=slider.scrollWidth){
+        slider.style.left=-(slider.scrollWidth-slider.clientWidth)+'px'
+        return
+    }else{
+        slider.style.left=(currentLeftPosition-interval)+'px'
+    }
+})
