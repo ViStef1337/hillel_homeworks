8c5daee33ede1a211550d2448ab382391edeccac
Vistef <stefanykvitalik1337@gmail.com>
2024-07-22 17:03:28 -0500

    rrrr

--- js/script.js
+++ js/script.js
@@ -22,3 +22,83 @@ let intervalId = setInterval(() => {
 }, 1000)
 
 //як передавати посилання на функцію як працює і приклади
+
+
+const arr = [1,2,1,2,2,3,3,4,4,5,5]
+
+const qwe = arr.filter((item,index,arr)=>arr.indexOf(item)===index)
+
+// const qwe = []
+// for (let i = 0;i<arr.length;i++){
+//     if (!qwe.includes(arr[i])){
+//         qwe.push(arr[i])
+//     }
+// }
+console.log(qwe)
+// arr.forEach((item,index,arr)=>{
+//     console.log(item);console.log(index);console.log(arr)
+// })
+
+
+// function ownForEach (arr,callBack){
+//     for (let i=0;i<=arr.length;i++){
+//         callBack(arr[i],i,arr)
+//     }
+// }
+//
+// ownForEach(arr,(item,index,arr)=>{
+//     console.log(item);console.log(index);console.log(arr)
+// })
+
+
+// const newArr = arr.map((item)=>{
+//     return item*2
+// })
+//
+// console.log(newArr)
+
+
+
+// function ownMap(arr,callBack){
+//     console.log(arr)
+//     const newArr = []
+//     for (let i=0;i<arr.length;i++){
+//         newArr.push(callBack(arr[i],i,arr))
+//     }
+//     return newArr
+// }
+
+// function ownFind(arr,callBack){
+//     for (let i=0;i<arr.length;i++){
+//         if (callBack(arr[i],i,arr)){
+//             return arr[i]
+//         }
+//     }
+//     return undefined
+// }
+//
+// console.log(ownMap(arr,(item,index,arr)=>{
+//     return item===3
+// }))
+// ownMap(arr,(item,index,arr)=>{
+//     return console.log(item);console.log(index);console.log(arr)
+// })
+
+// console.log(ownMap(arr,(item,index,arr)=>{
+//     return item*2
+// }))
+
+// function ownFilter(arr,callBack){
+//     const newArr = []
+//     for (let i=0;i<arr.length;i++){
+//         if (callBack(arr[i],i,arr)){
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+//
+// console.log(ownFilter(arr,(item,index,arr)=>{
+//     return item % 2 === 0
+// }))
+
